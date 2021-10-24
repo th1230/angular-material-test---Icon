6b140dcef9fbc741ac97e26090ba022ded05dd97
葉濬宇 <thomasyeayea@gmail.com>
2021-10-24 21:14:04 +0800

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+//解決xss的問題，需在module中import HttpClientModule
+import { MatIconRegistry } from '@angular/material/icon';
+//註冊第三方的icon圖案
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'angular-materialIcon';
+
+  constructor(
+    private MatIconRegistry: MatIconRegistry,
+    private DomSanitizer: DomSanitizer
+  ) {
+    //使用DI依賴注入後使用其方法
+
+    this.MatIconRegistry.addSvgIconInNamespace(
+      'custom-svg',
+      'angular',
+      this.DomSanitizer.bypassSecurityTrustResourceUrl(
+        'assets/icon/angular.svg'
+      )
+    );
+    //註冊svg圖案
+
+    this.MatIconRegistry.registerFontClassAlias('fontawesome', 'fa');
+    //註冊第三方icon圖示
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+//angular material 動畫模組
+import { MatIconModule } from '@angular/material/icon';
+//icon 管理模組，引入後可使用mat-icon來代替i標籤的功能
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    HttpClientModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
